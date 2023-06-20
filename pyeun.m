edb2c760c7e9e44e5b80df26e3e44e0233881cb8
Ong Wen Hui <111413221+Ongwenhui@users.noreply.github.com>
2023-06-20 13:42:41 +0800

    Delete pyeun.m

--- pyeun.m
+++
@@ -1,9 +0,0 @@
-pe = pyenv;
-if pe.Status == "Loaded" && pe.Version ~= "3.10"
-    disp('To change the Python version, restart MATLAB, then call pyenv(Version="3.10").')
-else
-    pyenv('Version', '/Library/Frameworks/Python.framework/Versions/3.10/bin/python3');
-end
-pyenv("ExecutionMode","OutOfProcess")
-values = [00001 true];
-pyrunfile("helloworld.py", values=values);
