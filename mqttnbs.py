e2d9f67cbcc1b88479dc6e0a193bdb766dfada21
Ong Wen Hui <111413221+Ongwenhui@users.noreply.github.com>
2023-06-22 14:30:16 +0800

    Update mqttnbs.py

--- mqttnbs.py
+++ mqttnbs.py
@@ -1,8 +1,3 @@
-'''
-Important notes: soundlooper() -> keeps playmasker() running
-playmasker() -> plays selected masker from predictions
-'''
-
 import json
 import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
 #from PIL import Image
@@ -16,16 +11,9 @@ import sys
 import threading
 import math
 import csv
-import pandas as pd
-import random
-from pydub import AudioSegment
-import keyboard
-import sys
-import tty
 import termios
 from awscrt import io, mqtt, auth, http
 from awsiot import mqtt_connection_builder
-
 varymaskers = False
 MEOW = False
 
@@ -94,7 +82,7 @@ calibgains = readcsv('/home/pi/mqtt_client/Calibrations_final_speaker.csv')
 # mqttRANGE = 20
 
 LOCATION_ID = 'PWP'
-optimaldistance = 1 #Punggol MSCP
+optimaldistance = 2.4
 numofspeakers = 4
 class soundplayer:
     def __init__(self):
@@ -110,11 +98,10 @@ class soundplayer:
         self.MQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(self.mqttCLIENT_ID)
         self.MQTTClient.configureEndpoint(self.mqttENDPOINT, 8883)
         self.MQTTClient.configureCredentials(self.mqttPATH_TO_AMAZON_ROOT_CA_1, self.mqttPATH_TO_PRIVATE_KEY, self.mqttPATH_TO_CERTIFICATE)
-        # self.msgdict = {'predictions': [{'rank': 1, 'id': 'bird_00075', 'gain': 0.01793866604566574, 'score': 0.47396957874298096}, {'rank': 2, 'id': 'bird_00075', 'gain': 0.020203232765197754, 'score': 0.47391098737716675}, {'rank': 3, 'id': 'bird_00075', 'gain': 0.02149977907538414, 'score': 0.47387757897377014}, {'rank': 4, 'id': 'bird_00069', 'gain': 0.05129025876522064, 'score': 0.4719516634941101}, {'rank': 5, 'id': 'bird_00075', 'gain': 0.10646381229162216, 'score': 0.47165820002555847}, {'rank': 6, 'id': 'bird_00069', 'gain': 0.10134433209896088, 'score': 0.4705403745174408}, {'rank': 7, 'id': 'bird_00025', 'gain': 0.0057005020789802074, 'score': 0.470207154750824}, {'rank': 8, 'id': 'bird_00069', 'gain': 0.15140660107135773, 'score': 0.46911484003067017}, {'rank': 9, 'id': 'bird_00025', 'gain': 0.08437956869602203, 'score': 0.46776485443115234}, {'rank': 10, 'id': 'bird_00071', 'gain': 0.06381088495254517, 'score': 0.4665457308292389}, {'rank': 11, 'id': 'bird_00069', 'gain': 0.25197258591651917, 'score': 0.46620792150497437}, {'rank': 12, 'id': 'bird_00046', 'gain': 0.07355550676584244, 'score': 0.46558114886283875}], 'base_score': 0.33999258279800415, 
-        # 'doa': self.currentdoa, 'from': 'ntu-gazebo01', 'timestamp': 1654067860.26, 'base_spl': 68.10846120156562} if not MEOW else {'predictions': [{'rank': 1, 'id': 'meow', 'gain': 1, 'score': 0.47396957874298096}, {'rank': 2, 'id': 'meow', 'gain': 1, 'score': 0.47391098737716675}, {'rank': 3, 'id': 'meow', 'gain': 1, 'score': 0.47387757897377014}, {'rank': 4, 'id': 'meow', 'gain': 1, 'score': 0.4719516634941101}, {'rank': 5, 'id': 'meow', 'gain': 1, 'score': 0.47165820002555847}], 'base_score': 0.33999258279800415, 
-        # 'doa': self.currentdoa, 'from': 'ntu-gazebo01', 'timestamp': 1654067860.26, 'base_spl': 68.10846120156562}
-        self.fixedmasker = 'playing fixed masker = '
-        self.msgdict = None
+        self.msgdict = {'predictions': [{'rank': 1, 'id': 'bird_00075', 'gain': 0.01793866604566574, 'score': 0.47396957874298096}, {'rank': 2, 'id': 'bird_00075', 'gain': 0.020203232765197754, 'score': 0.47391098737716675}, {'rank': 3, 'id': 'bird_00075', 'gain': 0.02149977907538414, 'score': 0.47387757897377014}, {'rank': 4, 'id': 'bird_00069', 'gain': 0.05129025876522064, 'score': 0.4719516634941101}, {'rank': 5, 'id': 'bird_00075', 'gain': 0.10646381229162216, 'score': 0.47165820002555847}, {'rank': 6, 'id': 'bird_00069', 'gain': 0.10134433209896088, 'score': 0.4705403745174408}, {'rank': 7, 'id': 'bird_00025', 'gain': 0.0057005020789802074, 'score': 0.470207154750824}, {'rank': 8, 'id': 'bird_00069', 'gain': 0.15140660107135773, 'score': 0.46911484003067017}, {'rank': 9, 'id': 'bird_00025', 'gain': 0.08437956869602203, 'score': 0.46776485443115234}, {'rank': 10, 'id': 'bird_00071', 'gain': 0.06381088495254517, 'score': 0.4665457308292389}, {'rank': 11, 'id': 'bird_00069', 'gain': 0.25197258591651917, 'score': 0.46620792150497437}, {'rank': 12, 'id': 'bird_00046', 'gain': 0.07355550676584244, 'score': 0.46558114886283875}], 'base_score': 0.33999258279800415, 
+        'doa': self.currentdoa, 'from': 'ntu-gazebo01', 'timestamp': 1654067860.26, 'base_spl': 68.10846120156562} if not MEOW else {'predictions': [{'rank': 1, 'id': 'meow', 'gain': 1, 'score': 0.47396957874298096}, {'rank': 2, 'id': 'meow', 'gain': 1, 'score': 0.47391098737716675}, {'rank': 3, 'id': 'meow', 'gain': 1, 'score': 0.47387757897377014}, {'rank': 4, 'id': 'meow', 'gain': 1, 'score': 0.4719516634941101}, {'rank': 5, 'id': 'meow', 'gain': 1, 'score': 0.47165820002555847}], 'base_score': 0.33999258279800415, 
+        'doa': self.currentdoa, 'from': 'ntu-gazebo01', 'timestamp': 1654067860.26, 'base_spl': 68.10846120156562}
+        # self.msgdict = None
         self.currentmasker = "bird_00075" if not MEOW else "meow"
         self.maskerpath = "/home/pi/mqtt_client/maskers/"
         self.maskergain = 1.2
@@ -124,10 +111,8 @@ class soundplayer:
         self.weightedgain = 0
         self.buffersize = 20
         self.blocksize = 4096
-        self.q = queue.Queue()#(maxsize=self.buffersize)
-        self.q2 = queue.Queue()
-        self.msgq = None
-        self.msgqeval = None
+        self.q = queue.Queue(maxsize=self.buffersize)
+        self.q2 =queue.Queue()
         self.event = threading.Event()
         self.fadelength = 80
         self.maskercounter = 0
@@ -135,20 +120,9 @@ class soundplayer:
         self.maskergainorig = 1
         self.doadiff = 20
         self.ambientspl = 68.1
-        self.nexttrackmsg = 'playback starting...'
-        self.beforeevaluationmsg = 'evaluation starting...'
-        self.duringevaluationmsg = 'evaluation in progress, press any key to proceed to the next track.'
-        self.endmsg = 'end of study'
-        self.voicePromptGain = 0.1
 
     def insitucompensate(self, numofspeakers,distance):
         compensated = round(20*math.log10(distance) - 10*math.log10(numofspeakers))
-        return compensated
-
-    def insituMultiMaskercompensate(self, numofspeakers,distance,noOfMaskers):
-        compensated = round(20*math.log10(distance) - 
-                            10*math.log10(numofspeakers) -
-                            20*math.log10(noOfMaskers))
         return compensated
 
     def spatialize(self, masker, angle, normalize=True, offset=-65, k=1.0):
@@ -202,54 +176,45 @@ class soundplayer:
         return masker4c        
 
     def msgcallback(self, client, userdata, message):
-        print("GETTING PREDICTIONS FROM AWS")
         incomingmsg=json.loads(message.payload.decode('utf-8'))
-        # self.q.put_nowait(incomingmsg)
-        # self.q2.put_nowait(incomingmsg)
-        self.msgq = incomingmsg
+        self.q2.put_nowait(incomingmsg)
+        print (incomingmsg)
+        #print(msgdict['predictions'])
         print("Recommended Masker is: " + str(incomingmsg['predictions'][0]["id"]))
         print("Recommended Gain is: {} ({}dB)".format(incomingmsg['predictions'][0]["gain"], interpolate(incomingmsg['predictions'][0]["id"],incomingmsg['predictions'][0]["gain"])))
         print("BaseSPL is: {}".format(incomingmsg["base_spl"]))
         # data, fs = sf.read(msgdict['predictions'][0]["id"]+'.wav', dtype='float32')  
         # sd.play(data, fs, device=1)
-
     def playsilence(self):
         print('playing silence')
-        silence, silencefs = sf.read(self.maskerpath + "bird_00075.wav", dtype='float32')
-        sd.play(silence, silencefs)
-        sd.wait()
-        if switch == 1:
-            pass
-
+        silence, silencefs = sf.read(self.maskerpath + "silence30s.wav", dtype='float32')
+        while True:
+            sd.play(silence, silencefs, device=1)
+            sd.wait()
+            if switch == 1:
+                break
     def playmasker(self):
-        print("playing masker")
         self.q = queue.Queue(maxsize=self.buffersize)
         newmasker = None
         newweightedgain = None
         newdoa = None
-        self.msgdict = self.msgqeval
+        self.q = queue.Queue(maxsize=self.buffersize)
+        print(self.msgdict)
         
-        #why q2?    
+        
         if not self.q2.empty():
-            self.msgdict = self.msgqeval
+            self.msgdict = self.q2.get_nowait()
             self.ambientspl = self.msgdict["base_spl"]
-
-        if self.msgdict != None: #If there is a prediction
+        if self.msgdict != None:
             print("starting playback again")
-            # maskercounter: to get index of the master from the dictionary, usually 0 for top masker
             print("masker counter=" + str(self.maskercounter))
-            # if the masker to be played is not self.currentmasker, set self.maskergain to the gain of the masker to be played
-            # self.currentmasker is set to bird_00075 by default
             if (self.msgdict['predictions'][self.maskercounter]["id"] != self.currentmasker) or (abs(self.msgdict['predictions'][self.maskercounter]["gain"]-self.maskergain)*self.gainweight>self.maskerdiff) or (abs(self.currentdoa - self.msgdict["doa"])>self.doadiff):
                 self.maskergain = self.msgdict['predictions'][self.maskercounter]["gain"]
-                # if self.maskergain (set in previous step) less than gainlimit (set at 1000)
                 if self.maskergain*self.gainweight < self.gainlimit:
                     print("self.maskergain = {}".format(self.maskergain))
                     # print("self.msgdict['predictions'][self.maskercounter]['id']+'.wav' = {}".format(self.msgdict['predictions'][self.maskercounter]["id"]+'.wav'))
                     # print("round(self.ambientspl + 20*math.log10(self.maskergain)) = {}".format(round(self.ambientspl + 20*math.log10(self.maskergain))))
-                    # calculate amssgain
                     amssgain = interpolate(self.msgdict['predictions'][self.maskercounter]["id"],self.maskergain) + self.insitucompensate(numofspeakers,optimaldistance)
-                    # set amssgaint to min 45 and max 83
                     if amssgain >45 and amssgain <= 83:
                         pass
                     elif amssgain >83:
@@ -260,31 +225,97 @@ class soundplayer:
                     print("self.weightedgain = {}".format(self.weightedgain))
                 else:
                     self.weightedgain = self.gainlimit
+                self.currentmasker = self.msgdict['predictions'][self.maskercounter]["id"]
                 self.currentdoa = self.msgdict["doa"]
                 # self.currentmaskerorig = self.currentmasker
         else:
             pass
-        try:
-            f, fs = sf.read(self.maskerpath + self.currentmasker +'.wav')
-            data = self.spatialize(f*self.weightedgain, self.currentdoa)
-            print(self.currentmasker)
-            print("Playing at self.weightedgain = {}".format(self.weightedgain))
-            compGain = math.pow(10,self.insitucompensate(numofspeakers,optimaldistance)/20)
-            print('Compensated gain: {} dB'.format(20*math.log10(compGain)))
-            sd.play(f*self.weightedgain*compGain, fs)
-            sd.wait()
-        except:
-            pass
-        if switch == 0:
-            pass
-      
+        with sf.SoundFile(self.maskerpath + (self.currentmasker if not MEOW else "meow") +'.wav') as f:
+            print("stream created using {} with gain: {} at DOA: {}".format(self.currentmasker,self.weightedgain,self.currentdoa))
+            stream = sd.OutputStream(
+                samplerate=f.samplerate, blocksize=self.blocksize,
+                device=1, channels=4,
+                callback=self.streamcallback, finished_callback=self.event.set)
+            with stream:
+                timeout = self.blocksize * self.buffersize / f.samplerate
+                for _ in range(int(self.buffersize)):
+                    data = self.spatialize(f.read(self.blocksize, always_2d=True)*self.weightedgain, self.currentdoa)
+                    if not len(data):
+                        break
+                    self.q.put_nowait(data)  # Pre-fill queue
+                    # print("queue pre-filled")
+                while len(data):
+                    if not self.q2.empty():
+                        self.msgdict = self.q2.get_nowait()
+                        
+                        if (self.msgdict['predictions'][0]["id"] != self.currentmaskerorig) or (abs(self.msgdict['predictions'][0]["gain"]-self.maskergainorig)*self.gainweight>self.maskerdiff) or (abs(self.currentdoa - self.msgdict["doa"])>self.doadiff):
+                            newgain = self.msgdict['predictions'][0]["gain"]
+                            newamssgain = interpolate(self.msgdict['predictions'][0]["id"],newgain) + self.insitucompensate(numofspeakers,optimaldistance)
+                            print("newamssgain = {}".format(newamssgain))
+                            if newamssgain >45 and newamssgain <= 83:
+                                pass
+                            elif newamssgain >83:
+                                newamssgain = 83
+                            elif newamssgain <46:
+                                newamssgain = 46
+                            newweightedgain = calibgains[self.msgdict['predictions'][self.maskercounter]["id"]+'.wav'][str(newamssgain)]
+
+                            newmasker = self.msgdict['predictions'][0]["id"]
+                            self.maskercounter=0
+                            print("masker counter=" + str(self.maskercounter))
+                            newdoa = self.msgdict['doa']
+                        else:
+                            pass
+                    else:
+                        pass
+                    if (newmasker != self.currentmasker and newmasker != None) or (newweightedgain != None and abs(newweightedgain-self.weightedgain)>self.maskerdiff) or (newdoa!=None and abs(self.currentdoa - newdoa)>self.doadiff):
+                        if MEOW:
+                            newmasker = "meow"
+                        if switch == 0:
+                            print('exiting playmasker')
+                            return
+                        fnew = sf.SoundFile(self.maskerpath + newmasker + '.wav')
+                        print("Changing - Playing: {} with gain:{} at DOA: {}".format(newmasker,newweightedgain,newdoa))
+                        for i in range(self.fadelength):
+                            track1 = self.spatialize(f.read(self.blocksize, always_2d=True)*(1-(i/self.fadelength))*self.weightedgain, self.currentdoa)
+                            track2 = self.spatialize(fnew.read(self.blocksize, always_2d=True)*(i/self.fadelength)*newweightedgain, newdoa)
+                            if len(track1) < len(track2) and not self.q.empty():
+                                track1cut = np.zeros((self.blocksize,4))
+                                track1cut[:len(track1)] = track1
+                                track1cut[len(track1):].fill(0)
+                                print("track ending")
+                            else:
+                                track1cut = track1
+                            data = track1cut + track2
+                            self.q.put(data, timeout=None)
+                        f = fnew
+                        self.currentmasker = newmasker
+                        self.currentmaskerorig = self.currentmasker
+                        self.weightedgain = newweightedgain
+                        self.maskergainorig=newgain
+                        self.currentdoa = newdoa
+
+                        # time.sleep(10)
+                    else:
+                        # print("reading data")
+                        data = self.spatialize(f.read(self.blocksize, always_2d=True)*self.weightedgain,self.currentdoa)
+                        self.q.put(data, timeout=None)
+                self.event.wait()  # Wait until playback is finished
+            if self.maskercounter<4 and varymaskers== True:
+                self.maskercounter+=1
+            else:
+                self.maskercounter=0
+            print("masker counter=" + str(self.maskercounter))
+            print("read done")
+            if switch == 0:
+                exit
     def streamcallback(self, outdata, frames, time, status):
         data = np.zeros((self.blocksize,1))
-        assert frames == self.blocksize
-        # if status.output_underflow:
-        #     print('Output underflow: increase blocksize?', file=sys.stderr)
-        #     raise sd.CallbackAbort
-        assert not status
+        # assert frames == self.blocksize
+        # # if status.output_underflow:
+        # #     print('Output underflow: increase blocksize?', file=sys.stderr)
+        # #     raise sd.CallbackAbort
+        # assert not status
         # try:
         data = self.q.get_nowait()
         # except queue.Empty as e:
@@ -296,45 +327,23 @@ class soundplayer:
             raise sd.CallbackStop
         else:
             outdata[:] = data
-
-
     def soundlooper(self):
-        #wait for predictions to load first
-        notprinted_flag = True
-        while self.msgq == None:
-            if notprinted_flag:
-                print("Waiting for predictions")
-                notprinted_flag = False
-        print('predictions loaded')
         while True:
+            print(switch)
             if switch == 1:
                 while True:
                     self.playmasker()    
                     if switch == 0:
                         break 
             if switch == 0:
-                while True:
-                    self.playsilence()
-                    if switch == 1:
-                        break
-
-
- 
+                self.playsilence()
     def mqttlooper(self):
         while True:
-            print("MQTTLOOPER RUNNING")
             self.MQTTClient.subscribeAsync(self.mqttTOPIC, 0,messageCallback = self.msgcallback)
             time.sleep(10)
-    
 def iotlooper():
     while True:
         iotClient.subscribeAsync(TOPIC, 0,messageCallback = customcallback)
-   
-def is_key_pressed():
-    tty.setraw(sys.stdin.fileno())
-    key = sys.stdin.read(1)
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key != ''
 
 sp = soundplayer()
 
@@ -371,14 +380,12 @@ def customcallback(client, userdata, message):
     splitpayload = payloadstr.split('onoff')
     global switch
     switch = int(splitpayload[1][3])
-    print(switch)
     
 iotClient.connectAsync()
 print("Connected to test/nbs")
-switch = 0
+switch = 1
 
 settings = termios.tcgetattr(sys.stdin)
-
 mqtt_thread = threading.Thread(
         target=sp.mqttlooper,
         name="mqtt",
@@ -401,5 +408,7 @@ soundlooper_thread.start()
 soundlooper_thread.join()
 mqtt_thread.join()
 iot_thread.join()
+# mqtt_thread.join()
+
 # sp.MQTTClient.subscribeAsync(sp.mqttTOPIC, 0,messageCallback = sp.msgcallback)
 # sp.playmasker()
